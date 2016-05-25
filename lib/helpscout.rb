39b1d3112a4fb0a07817a9f51be017888f647f6f
Mark Mulder <markmulder@gmail.com>
2016-05-25 12:31:20 +0200

    Update code style

--- lib/helpscout.rb
+++ lib/helpscout.rb
@@ -5,7 +5,9 @@ module Helpscout
     attr_accessor :last_response
 
     def initialize(api_key)
-      throw ArgumentError "API key is required" if api_key.nil?
+      if api_key.nil?
+        raise ArgumentError "API key is required"
+      end
 
       @api_key = api_key
     end
@@ -13,17 +15,17 @@ module Helpscout
     def post(path, options = {})
       options[:body] = options[:body].to_json if options[:body]
 
-      request :post, path, options
+      request(:post, path, options)
     end
 
     def put(path, options = {})
       options[:body] = options[:body].to_json if options[:body]
 
-      request :put, path, options
+      request(:put, path, options)
     end
 
     def get(path, options = {})
-      request :get, path, options
+      request(:get, path, options)
     end
 
     def search(path, query, page_id = 1, items = [])
@@ -49,7 +51,7 @@ module Helpscout
     #
     # Returns conversation id from HS
     def create_conversation(data)
-      post "conversations", { body: data }
+      post("conversations", { body: data })
 
       conversation_uri = last_response.headers["location"]
       conversation_uri.match(/(\d+)\.json$/)[1].to_i
@@ -63,25 +65,26 @@ module Helpscout
     #
     # Returns hash from HS with conversation data
     def get_conversation(id)
-      get "conversations/#{id}"
+      get("conversations/#{id}")
     end
 
     # http://developer.helpscout.net/help-desk-api/conversations/update/
     def update_conversation(id, data)
-      put "conversations/#{id}", { body: data }
+      put("conversations/#{id}", { body: data })
     end
 
     def get_conversation(id)
-      get "conversations/#{id}"
+      get("conversations/#{id}")
     end
 
     def get_customer_from_conversation(conversation)
-      get "customers/#{conversation["item"]["customer"]["id"]}"
+      customer_id = conversation["item"]["customer"]["id"]
+      get("customers/#{customer_id}")
     end
 
     # http://developer.helpscout.net/help-desk-api/search/conversations/
     def search_conversations(query)
-      search "search/conversations", query
+      search("search/conversations", query)
     end
 
     protected
@@ -89,9 +92,15 @@ module Helpscout
     def request(method, path, options)
       uri = URI("https://api.helpscout.net/v1/#{path}.json")
 
+      # The password can be anything, it's not used, see:
+      # http://developer.helpscout.net/help-desk-api/
       options.reverse_merge!({
-        basic_auth: { username: @api_key, password: 'X' },
-        headers: { 'Content-Type' => 'application/json' }
+        basic_auth: {
+          username: @api_key, password: 'X'
+        },
+        headers: {
+          'Content-Type' => 'application/json'
+        }
       })
 
       @last_response = HTTParty.send(method, uri, options)
